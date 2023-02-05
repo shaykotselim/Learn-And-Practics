b0b6d002ddecf47f039ff68e241f1b958572efcc
shaykot43500 <hossainshaykot@gmail.com>
2023-02-05 21:01:49 +0600

    added comment

--- firstProgram/first.c
+++ firstProgram/first.c
@@ -1,3 +1,10 @@
+        /*
+            Name: Shaykot Hossain selim
+            Depertment: Computer Science And Engineering
+            Batch: 59;
+            Section: A
+            ID: 2231091023;
+        */
 
     #include<stdio.h>
 
@@ -5,7 +12,7 @@
             printf("        T  H          E  L\n");
             printf("       O    O        S    I\n");
             printf("S     K      S      N      M\n");
-            printf(" H   Y        S    I   \n");
+            printf(" H   Y        S    I         \n");
             printf("   A            A");
 
             getch();
