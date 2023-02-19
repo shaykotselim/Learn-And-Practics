3976acce1ebc173db75e80c0cb173f5bafa3389a
shaykot43500 <hossainshaykot@gmail.com>
2023-02-19 14:26:46 +0600

    added escape sequence

---
+++ Escape-sequence/escapeSequence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    int main(){
+        printf("\a"); // for alarm;
+        printf("\b"); // backSpace;
+        printf("\f"); // Form feed(new page);
+        printf("\n"); // New line;
+        printf("\c"); // Carriage return;
+        printf("\t"); // horizontal tab;
+        printf("\v"); // vertical tap;
+        printf("\`"); // single Quotation mark;
+        printf("\""); // double Quotation mark;
+        printf("\?"); // Question mark;
+        printf("\\"); // back slash;
+
+        getch();
+    }
+
